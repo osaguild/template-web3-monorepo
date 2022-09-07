194ee783514af2c30dbe8644aab42bd9ee097d37
osaguild <nobuhisa.abe.0922@gmail.com>
2022-09-07 22:11:20 +0900

    add interact with blockchain

---
+++ interface/config/address.ts
@@ -0,0 +1,5 @@
+const address = {
+  MESSAGE_CONTRACT: '0x913E7686B0B92c815D96Ed4B79a93E42DDFa8d06',
+}
+
+export { address }
---
+++ interface/hooks/useContents.ts
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react'
+import { useWeb3React } from '@web3-react/core'
+import { providers } from 'ethers'
+import { Message__factory } from '../typechain-types'
+import { address } from '../config/address'
+
+const useContents = () => {
+  const [contents, setContents] = useState<string[]>([])
+  const { library } = useWeb3React<providers.Web3Provider>()
+  console.log('useContents is called')
+
+  useEffect(() => {
+    console.log('library is ', library)
+    if (library) {
+      const contract = Message__factory.connect(address.MESSAGE_CONTRACT, library.getSigner())
+      contract.getMessageLength().then((length) => {
+        const ids = [...Array(length.toNumber()).keys()].map((i) => i + 1)
+        const promises = ids.map((id) => contract.selectMessage(id))
+        Promise.all(promises).then((message) => setContents(message))
+      })
+    }
+    console.log('contents', contents)
+  }, [library])
+
+  return contents
+}
+
+export { useContents }
