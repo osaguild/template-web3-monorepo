ce9f678b26854cef318c3528b5736ee8428278d8
osaguild <nobuhisa.abe.0922@gmail.com>
2022-09-07 22:18:17 +0900

    add test

--- interface/components/Content/index.test.tsx
+++ interface/components/Content/index.test.tsx
@@ -10,11 +10,4 @@ describe('test', () => {
   it('text check', () => {
     expect(screen.queryByTestId('text')).toHaveTextContent('welcome to web3 template')
   })
-  it('item check', () => {
-    expect(screen.queryByTestId('item-1')).toHaveTextContent('Box 1')
-    expect(screen.queryByTestId('item-2')).toHaveTextContent('Box 2')
-    expect(screen.queryByTestId('item-3')).toHaveTextContent('Box 3')
-    expect(screen.queryByTestId('item-4')).toHaveTextContent('Box 4')
-    expect(screen.queryByTestId('item-5')).toHaveTextContent('Box 5')
-  })
 })
